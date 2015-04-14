b044e2f1eb9db7b2bd5c5fb314fd65364fc143db
Eric Rasch <ericrasch@gmail.com>
2015-04-14 12:06:21 -0500

    Added environment detection

--- web/inc-header.php
+++ web/inc-header.php
@@ -1,1 +1,11 @@
-<link rel="stylesheet" href="assets/dist/style.min.css">
+<?php
+  if ( true !== strpos( $_SERVER['REQUEST_URI'], 'ericrasch.com' ) ) {
+    define( 'PROD_ENV', 'local' );
+  }
+
+  if ( 'local' != PROD_ENV ) {
+    ?>
+  <link rel="stylesheet" href="http://ericrasch.com/assets/dist/style.min.css">
+  <?php } else { ?>
+  <link rel="stylesheet" href="assets/dev/style.css">
+<?php } ?>
